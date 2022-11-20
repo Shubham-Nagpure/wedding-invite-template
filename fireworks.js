d94dec0cd9e25d25066bfa175f4a4d6c3effe1ce
anigore <aniket.p.gore@gmail.com>
2022-11-21 02:21:24 +0530

    all animation and buttons implementations

--- fireworks.js
+++ fireworks.js
@@ -9,62 +9,62 @@ const particleCount = 60;
 
 // Color generate
 const randomValue = (max, min) => Math.floor(Math.random() * (max - min) + min);
-const randomColor = () => 
-  `rgba(${randomValue(255, 45)}, ${randomValue(255, 45)}, ${randomValue(255, 45)}, ${randomValue(100, 50)}%)`;
+const randomColor = () =>
+    `rgba(${randomValue(255, 45)}, ${randomValue(255, 45)}, ${randomValue(255, 45)}, ${randomValue(100, 50)}%)`;
 
 // Genterate particle
 const randomPosition = (max, min) => ({
-  x: `${randomValue(max, min)}%`,
-  y: `${randomValue(max, min)}%`
+    x: `${randomValue(max, min)}%`,
+    y: `${randomValue(max, min)}%`
 });
 const createParticle = color => `radial-gradient(circle, ${color} 0.2vmin, #0000 0)`;
 
 // Genterate particle style
 const particleStyle = (particles, positions) => ({
-  backgroundImage: particles.join(','),
-  backgroundPositionX: positions.x.join(',').toString(),
-  backgroundPositionY: positions.y.join(',').toString()
+    backgroundImage: particles.join(','),
+    backgroundPositionX: positions.x.join(',').toString(),
+    backgroundPositionY: positions.y.join(',').toString()
 });
 
 // Generate firework
 const createFirework = (firework, n) => {
-  var particles = [];
-  var positions = {
-    x: [],
-    y: []
-  };
-  
-  const color = randomColor();
-  for(let i = 0; i < n; i++) {
-    const {x, y} = randomPosition(100, 0);
-    particles.push(createParticle(color));
-    positions.x.push(x);
-    positions.y.push(y);
-  }
-  
-  firework.css({
-    ...particleStyle(particles, positions),
-    top: `${randomValue(50, 20)}%`,
-    left: `${randomValue(80, 30)}%`
-  });
+    var particles = [];
+    var positions = {
+        x: [],
+        y: []
+    };
+
+    const color = randomColor();
+    for (let i = 0; i < n; i++) {
+        const { x, y } = randomPosition(100, 0);
+        particles.push(createParticle(color));
+        positions.x.push(x);
+        positions.y.push(y);
+    }
+
+    firework.css({
+        ...particleStyle(particles, positions),
+        top: `${randomValue(50, 20)}%`,
+        left: `${randomValue(80, 30)}%`
+    });
 };
 
 // Animate settings
 const duration = second => second * 1000;
 const effect = () => ({
-  opacity: 1,
-  width: '30vmin'
+    opacity: 1,
+    width: '30vmin'
 });
 const reset = () => ({
-  width: ''
+    width: ''
 });
 
 const animate = (firework, time) => {
-  createFirework(firework, particleCount);
-  firework.delay(time).animate(effect(), duration(1.2), 'swing', () => {
-    firework.css(reset());
-    animate(firework, time);
-  })
+    createFirework(firework, particleCount);
+    firework.delay(time).animate(effect(), duration(1.2), 'swing', () => {
+        firework.css(reset());
+        animate(firework, time);
+    })
 };
 
 animate($Firework1, duration(0));
